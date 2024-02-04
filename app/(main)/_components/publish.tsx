3a06ff105782cd57ee657ef62723fcc83a177f64
rBhagat4196 <rahulbhagat13slr@gmail.com>
2024-02-04 19:00:26 +0530

    publish page implementing

---
+++ app/(main)/_components/publish.tsx
@@ -0,0 +1,9 @@
+import { Doc } from "@/convex/_generated/dataModel";
+
+interface PublishProps {
+  initialData: Doc<"documents">;
+}
+
+export const Publish = ({ initialData }: PublishProps) => {
+  return <>Publish</>;
+};
