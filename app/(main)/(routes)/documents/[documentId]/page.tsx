fcb986c3efd17b5fc4441e7191cb8f40036f328c
rBhagat4196 <rahulbhagat13slr@gmail.com>
2024-02-04 17:57:26 +0530

    cover image skeleton

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -4,6 +4,7 @@ import { Id } from "@/convex/_generated/dataModel";
 import { useQuery } from "convex/react";
 import { Toolbar } from "@/components/toolbar";
 import { Cover } from "@/components/cover";
+import { Skeleton } from "@/components/ui/skeleton";
 interface DocumentIdPageProps {
   params: {
     documentId: Id<"documents">;
@@ -20,9 +21,22 @@ const DocumentIdPage = ({
     documentId : params.documentId
   });
 
-  if(document === undefined){
-    return <p>Loading...</p>
+  if (document === undefined) {
+    return (
+      <div>
+        <Cover.Skeleton />
+        <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
+          <div className="space-y-4 pl-8 pt-4">
+            <Skeleton className="h-14 w-[50%]" />
+            <Skeleton className="h-4 w-[80%]" />
+            <Skeleton className="h-4 w-[40%]" />
+            <Skeleton className="h-4 w-[60%]" />
+          </div>
+        </div>
+      </div>
+    );
   }
+  
   if(document ===  null) return null
 
   return (
