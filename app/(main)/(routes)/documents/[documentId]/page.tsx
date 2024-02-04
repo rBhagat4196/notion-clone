b1c7266cbad309a0e355478656922fddfd8dd016
rBhagat4196 <rahulbhagat13slr@gmail.com>
2024-02-04 15:24:55 +0530

    remove icon

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,7 +1,8 @@
+"use client"
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useQuery } from "convex/react";
-
+import { Toolbar } from "@/components/toolbar";
 
 interface DocumentIdPageProps {
   params: {
@@ -27,7 +28,7 @@ const DocumentIdPage = ({
   return (
     <div className="pb-40">
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
-
+      <Toolbar initialData={document} />
       </div>
     </div>
   )
